8de3d9882520799b4af60babfe96b6746aea46b7
Sarah <Sarah@HP-SARAHI>
2023-12-12 21:50:03 -0600

    Agregar archivos de proyecto.

---
+++ Interfaces/Algorithms/ImethodAlgorithms.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class ImethodAlgorithms(ABC):
+    @abstractmethod
+    def sort(self, array):
+        pass
+
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from OperationsMain import OperationMain
+
+
+if __name__ == '__main__':
+    OperationMain.main_ds()
+
+
+
